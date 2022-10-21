eb2bfa52d2bc01465964e443447a452d92ccb164
michael <2018118090@ub.edu.bz>
2022-10-20 21:25:52 -0600

    finised errors.go

--- backend/cmd/api/errors.go
+++ backend/cmd/api/errors.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -20,4 +21,45 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 		app.logError(r, err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
+}
+
+// Server error response
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	//We log the error
+	app.logError(r, err)
+
+	//prepare a message with the error
+	message := "the server encountered a problem and could not process the request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+// The not found response
+func (app *application) notFoundReponse(w http.ResponseWriter, r *http.Request) {
+	//Create our message
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+// A method not allowed response
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	//Create our message
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+// User provide a bad request
+func (app *application) badRequestResonse(w http.ResponseWriter, r *http.Request, err error) {
+	//Create our message
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+// Validation error
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
+// Edut conflict error
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
