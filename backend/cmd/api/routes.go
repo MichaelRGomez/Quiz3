4dfbe06793024899764e00ae545d612f64410917
michael <2018118090@ub.edu.bz>
2022-10-22 15:18:20 -0600

    finised all the handler functions and made sure all routes are correctly linked

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -11,11 +11,11 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	//security routes
-	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.NotFound = http.HandlerFunc(app.notFoundReponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	//actual routes
-	router.HandlerFunc(http.MethodGet, "/v1/todo", app.showTasksHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/todo", app.listTasksHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/todo", app.createTaskHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/todo/:id", app.showTaskHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/todo/:id", app.updateTaskHandler)
